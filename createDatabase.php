7bdd635b33ec6d9a74200131adc43de7102abf02
Madhav Arora <madhavarora200193@gmail.com>
2020-08-09 04:21:10 +0530

    Fixing the 'Create Database' file

--- createDatabase.php
+++ createDatabase.php
@@ -1,17 +1,23 @@
 <?php
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "animesucker";
+    $dbServernameNew = "localhost";
+    $dbUsernameNew = "root";
+    $dbPasswordNew = "animesucker";
     
-    $conn = mysqli($dbServername, $dbUsername, $dbPassword);
+    $connNew = mysqli_connect($dbServernameNew, $dbUsernameNew, $dbPasswordNew);
     
-    if (!$conn) {
+    if (!$connNew) {
         die("connection failed: ".connect_error());
     }
 
     //create database
     $sqlDatabase = "CREATE DATABASE weeb_sauce";
-    $result = mysqli_query($conn, $sqlDatabase);
+    $result1 = mysqli_query($connNew, $sqlDatabase);
+
+    $connNew->close();
+?>
+
+<?php
+    include_once 'dbh.inc.php';
 
     //create users table
     $sqlUsers = "CREATE TABLE users (
@@ -25,14 +31,14 @@
         user_interests varchar(256) not null,
         user_pwd LONGTEXT not null,
         user_sub varchar(256))";
-    $result = mysqli_query($conn, $sqlUsers);
+    $result2 = mysqli_query($conn, $sqlUsers);
 
     //create services table
     $sqlServices = "CREATE TABLE services (
         service_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
         service_name varchar(256) not null,
         service_price varchar(256) not null)";
-    $result = mysqli_query($conn, $sqlServices);
+    $result3 = mysqli_query($conn, $sqlServices);
 
     //create queries table
     $sqlQueries = "CREATE TABLE queries (
@@ -42,27 +48,30 @@
         user_email varchar(256) not null,
         user_subject varchar(256) not null,
         user_message text(1000) not null)";
-    $result = mysqli_query($conn, $sqlQueries);
+    $result4 = mysqli_query($conn, $sqlQueries);
+?>
 
+<?php
     //adding admin user
     $sqlAdmin = "INSERT INTO users 
     (user_time, user_firstname, user_lastname, user_fullname, user_email, user_address, user_pwd) 
     VALUES (now(), 'Admin', 'User', 'Admin User', 'admin@email.com', 'India', 'admin')";
-    $result = mysqli_query($conn, $sqlAdmin);
+    $result5 = mysqli_query($conn, $sqlAdmin);
 
     //adding services
     $sqlService1 = "INSERT INTO `services`(`service_name`, `service_price`) VALUES ('Starter Mystery Box','Rs. 4,500 per month')";
-    $result = mysqli_query($conn, $sqlService1);
+    $result6 = mysqli_query($conn, $sqlService1);
 
     $sqlService2 = "INSERT INTO `services`(`service_name`, `service_price`) VALUES ('The Mystery Box','Rs. 10,000 per month')";
-    $result = mysqli_query($conn, $sqlService2);
+    $result7 = mysqli_query($conn, $sqlService2);
 
     $sqlService3 = "INSERT INTO `services`(`service_name`, `service_price`) VALUES ('Veteran Mystery Box','Rs. 18,000 per month')";
-    $result = mysqli_query($conn, $sqlService3);
+    $result8 = mysqli_query($conn, $sqlService3);
 
     //adding one query
     $sqlMessage = "INSERT INTO `queries`(`query_time`, `query_user`, `query_email`, `query_subject`, `query_message`) 
         VALUES (now(),'Someone','someone@email.com','Service Review','Your services are amazing and i will it recommend my friends too.')";
-    $result = mysqli_query($conn, $sqlAdmin);
+    $result9 = mysqli_query($conn, $sqlAdmin);
 
-    header("Location: ../index.php?database=success");
+    header("Location: index.php?database=success");
+?>
